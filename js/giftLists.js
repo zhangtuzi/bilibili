230534591e0d2ac548bf767a7e40cc059c3ea35a
zhangtuzi <1243933019@qq.com>
2017-05-22 21:11:39 +0800

    '缺少规则弹层'

--- js/giftLists.js
+++ js/giftLists.js
@@ -1,4 +1,9 @@
 
 $(function(){
-  var win_height=$(window).height();
+  var sw_height=$('.sw_jp_tc').height();
+  $('.sw_jp_tc').css('margin-top',-sw_height/2);
+  var xn_height=$('.xn_jp_tc').height();
+  $('.xn_jp_tc').css('margin-top',-xn_height/2);
+  var error_height=$('.error_tc').height();
+  $('.error_tc').css('margin-top',-xn_height/2);
 })
